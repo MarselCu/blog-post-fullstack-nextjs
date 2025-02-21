5cd4b0945d9b06ca7ed86a21d21160592ca545d7
kael <marchel.development@gmail.com>
2025-02-21 13:43:17 +0700

    chore: adding User interface to access all post

---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { Home, PlusCircle } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center p-4 shadow-md bg-white">
+      {/* Logo */}
+      <div className="text-xl font-bold">MyApp</div>
+
+      {/* Navigation Menu */}
+      <div className="flex gap-6">
+        <Link href="/" className="flex items-center gap-2 text-gray-700 hover:text-black">
+          <Home size={20} /> Home
+        </Link>
+        <Link href="/add-post" className="flex items-center gap-2 text-gray-700 hover:text-black">
+          <PlusCircle size={20} /> Add Post
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
